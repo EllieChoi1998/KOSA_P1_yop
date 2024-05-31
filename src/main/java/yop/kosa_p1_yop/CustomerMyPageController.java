3c106050e78aaf8051eb4b71815afa82b6a6d3cb
isolated-hwan <sksekwjdghks@hanmail.net>
2024-05-31 17:00:52 +0900

    오더에서 피자 선택 및 팝업 커스텀피자 토글버튼활용하여 화면 전환

--- src/main/java/yop/kosa_p1_yop/CustomerMyPageController.java
+++ src/main/java/yop/kosa_p1_yop/CustomerMyPageController.java
@@ -1,9 +1,13 @@
 package yop.kosa_p1_yop;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -17,4 +21,45 @@ public class CustomerMyPageController extends CustomerMainController{
         Scene scene = new Scene(fxmlLoader.load(), 450, 820);
         stage.setScene(scene);
     }
+
+    @FXML
+    private void handleMoveToStandardButtonAction() throws IOException {
+        Stage stage = (Stage) AppMain.getPrimaryStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AppOrderStandard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 820);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void handleMoveToCustomButtonAction() throws IOException {
+        Stage stage = (Stage) AppMain.getPrimaryStage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AppOrderCustom.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 820);
+        stage.setScene(scene);
+    }
+    @FXML
+    private void popup(ActionEvent event) {
+        try {
+            // calories.fxml 파일 로드
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AppCaloriesPage.fxml"));
+            Parent root = loader.load();
+
+            // 현재의 스테이지 얻기
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UNDECORATED); // 타이틀 바 제거
+            stage.setScene(new Scene(root));
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleCloseButtonAction(ActionEvent event) {
+        // 현재 창을 닫기
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
 }
